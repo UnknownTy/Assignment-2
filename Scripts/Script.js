4898a83aae0f5232b47c298a472323ac9feecdff
Unknown <Ty@gimpel.ca>
2020-12-06 16:39:02 -0800

    Added functionality to 'new note' and 'cancel' buttons.

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -2,6 +2,7 @@ let theme_button = document.getElementById('change-theme')
 let new_button = document.getElementById('new-note')
 let save_button = document.getElementById('save')
 let cancel_button = document.getElementById('cancel')
+let text_area = document.getElementById('note_input')
 
 theme_button.onclick = function(){
     let dark_elements = [
@@ -25,5 +26,22 @@ theme_button.onclick = function(){
         theme_button.innerText = 'Light Theme'
     } else{
         theme_button.innerText = 'Dark Theme'
+    }
+}
+
+cancel_button.onclick = function(){
+    cancel_button.toggleAttribute('hidden', true)
+    text_area.toggleAttribute('hidden', true)
+
+    save_button.toggleAttribute('hidden', true)
+}
+
+new_button.onclick = function(){
+    if(text_area.hasAttribute('hidden')){
+        cancel_button.toggleAttribute('hidden', false)
+        text_area.toggleAttribute('hidden', false)
+        save_button.toggleAttribute('hidden', false)
+    } else{
+        text_area.value = ""; // Clears the text_area
     }
 }
